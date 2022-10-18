2aec5b0a1f7002e0554999f55506716271c4c453
epike <epikeran@gmail.com>
2022-10-18 14:17:10 +0900

    feat: JDBC with Tdd

--- src/main/java/org/jdbc/ConnectionManager.java
+++ src/main/java/org/jdbc/ConnectionManager.java
@@ -3,16 +3,51 @@ package org.jdbc;
 import com.zaxxer.hikari.HikariDataSource;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class ConnectionManager {
 
-    public static DataSource getDataSource() {
+    public static final String DB_DRIVER = "org.h2.Driver";
+    public static final String DB_URL = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
+    public static final int MAX_POOL_SIZE = 40;
+
+    private static final DataSource ds;
+
+    static {
         HikariDataSource hikariDataSource = new HikariDataSource();
-        hikariDataSource.setDriverClassName("org.h2.Driver");
-        hikariDataSource.setJdbcUrl("jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1");
+        hikariDataSource.setDriverClassName(DB_DRIVER);
+        hikariDataSource.setJdbcUrl(DB_URL);
         hikariDataSource.setUsername("jp");
         hikariDataSource.setPassword("");
+        hikariDataSource.setMaximumPoolSize(MAX_POOL_SIZE); // 모니터링을 해서 적절한 풀사이즈를 정해야함.
+        hikariDataSource.setMinimumIdle(MAX_POOL_SIZE);
 
-        return hikariDataSource;
+        ds = hikariDataSource;
+    }
+
+//    private static Connection getConnection() throws SQLException {
+//        String url = "jdbc:h2:mem://localhost/~/jdbc-practice;MODE=MySQL;DB_CLOSE_DELAY=-1";
+//        String id = "jp";
+//        String pw = "";
+//        try {
+//            Class.forName("org.h2.Driver");
+//            return DriverManager.getConnection(url, id, pw);
+//        } catch (Exception ex) {
+//            return null;
+//        }
+//        return getDataSource().getConnection();
+//    }
+
+    public static Connection getConnection() {
+        try {
+            return ds.getConnection();
+        } catch (SQLException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    public static DataSource getDataSource() {
+        return ds;
     }
 }
